0e0631d67816cf87dbd63817b660c446ba52b74d
Leo Torres <leo.htor@gmail.com>
2018-03-23 18:29:24 -0400

    Initial logic to check if cards match

--- app.js
+++ app.js
@@ -30,12 +30,31 @@ for (var i = 0; i < list.length; i++) {
   // console.log($tile);
 }
 
-
+let openCards = [];
 document.addEventListener('click', function(e) {
+
+  let selected = e.target;
   if ( e.target.classList.contains('square') ) {
     // console.log(showTile);
     timeElapsed.style.visibility = 'visible';
-    let showTile = e.target.firstChild
+
+    let showTile = e.target.firstChild;
     showTile.style.visibility = 'visible';
+
+    openCards.push(selected);
+    console.log(openCards); 
+
+    if (openCards.length > 1) {
+
+      if (openCards[0].firstChild.innerHTML === openCards[1].firstChild.innerHTML) {
+        console.log('winner!');
+        openCards = [];
+      }
+      else {
+        openCards[0].firstChild.style.visibility = 'hidden';
+        openCards[1].firstChild.style.visibility = 'hidden';
+        openCards = [];
+      }
+    }
   };
 }, false);
