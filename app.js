b86a04af4e661be4ea9a4414f299df5bf8019260
Leo Torres <leo.htor@gmail.com>
2018-03-23 16:11:51 -0400

    Dynamically adding tiles to gameboard with random figures assigned

--- app.js
+++ app.js
@@ -1,7 +1,37 @@
+let list = ['face', 'face', 'wb_sunny', 'wb_sunny', 'beach_access', 'beach_access', 'star_border', 'star_border', 'filter_hdr', 'filter_hdr', 'local_florist', 'local_florist', 'flash_on', 'flash_on', 'palette', 'palette'];
 const tile = document.getElementsByClassName('square');
+const gameBoard = document.getElementById('game-board');
+
+// Shuffle function from http://stackoverflow.com/a/2450976
+function shuffle(array) {
+    var currentIndex = array.length, temporaryValue, randomIndex;
+
+    while (currentIndex !== 0) {
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+    }
+
+    return array;
+}
+shuffle(list);
+
+for (var i = 0; i < list.length; i++) {
+  let $square = document.createElement('div');
+  $square.classList.add('square');
+  let $tile = document.createElement('i');
+  $tile.classList.add('material-icons');
+  $tile.innerHTML = list[i];
+  $square.append($tile);
+  gameBoard.append($square);
+  // console.log($tile);
+}
+
 
 document.addEventListener('click', function(e) {
   if ( e.target.classList.contains('square') ) {
     console.log('clicked!');
   };
-}, false)
+}, false);
